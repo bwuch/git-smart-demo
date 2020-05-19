f096b40089d5b32da5172296d458823dfda063f2
Brian Wuchner <brian.wuchner@gmail.com>
2020-05-19 07:08:43 -0400

    Create MyPowershellScript.ps1

---
+++ MyPowershellScript.ps1
@@ -0,0 +1,8 @@
+#This is my powershell script.
+#This is version 1 and may have some problems, like this one:
+Get-Modules -ListAvailable
+
+# I bet getting all processes would be as easy as
+Get-Processes
+
+#This is the end of my script.
