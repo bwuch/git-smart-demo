89914f274e7a35e55b99dff99a43f35d642c13eb
Andy B <abidlen@users.noreply.github.com>
2020-05-21 08:58:01 -0400

    Update MyPowershellScript.ps1
    Fixes #1

--- MyPowershellScript.ps1
+++ MyPowershellScript.ps1
@@ -1,6 +1,6 @@
 #This is my powershell script.
 #This is version 1 and may have some problems, like this one:
-Get-Modules -ListAvailable
+Get-Module -ListAvailable
 
 # I bet getting all processes would be as easy as
 Get-Processes
